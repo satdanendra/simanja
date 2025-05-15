fd6a19b92d6d09f57e04892c0099aad5b7b7e02c
222112354 <danendra3671@gmail.com>
2025-05-15 08:18:10 +0700

    percobaan 2 google drive

--- config/app.php
+++ config/app.php
@@ -123,4 +123,9 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'providers' => [
+        // Lainnya...
+        App\Providers\GoogleDriveServiceProvider::class,
+    ],
+
 ];
