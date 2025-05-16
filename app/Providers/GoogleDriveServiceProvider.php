86281b30c9874af6d05144eda80babb5db864e59
222112354 <danendra3671@gmail.com>
2025-05-16 19:55:25 +0700

    percobaan 3 google drive

--- app/Providers/GoogleDriveServiceProvider.php
+++ app/Providers/GoogleDriveServiceProvider.php
@@ -2,20 +2,34 @@
 
 namespace App\Providers;
 
-use Google_Client;
-use Google_Service_Drive;
+// use Google\Client;
+// use Google\Service\Drive;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class GoogleDriveServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton(Google_Client::class, function ($app) {
-            $client = new Google_Client();
+        // Jangan load Google Drive saat sedang di CLI (misal php artisan)
+        if (App::runningInConsole()) {
+            return;
+        }
+
+        if (!class_exists(\Google\Client::class)) {
+            return;
+        }
+
+        if (!config('google-drive.client_id') || !config('google-drive.client_secret')) {
+            return;
+        }
+
+        $this->app->singleton(\Google\Client::class, function ($app) {
+            $client = new \Google\Client();
             $client->setClientId(config('google-drive.client_id'));
             $client->setClientSecret(config('google-drive.client_secret'));
             $client->setRedirectUri(url('/auth/google/callback'));
-            $client->setScopes([Google_Service_Drive::DRIVE]);
+            $client->setScopes([\Google\Service\Drive::DRIVE]);
             $client->setAccessType('offline');
             $client->setPrompt('consent');
 
@@ -26,9 +40,9 @@ class GoogleDriveServiceProvider extends ServiceProvider
             return $client;
         });
 
-        $this->app->singleton(Google_Service_Drive::class, function ($app) {
-            $client = $app->make(Google_Client::class);
-            return new Google_Service_Drive($client);
+        $this->app->singleton(\Google\Service\Drive::class, function ($app) {
+            $client = $app->make(\Google\Client::class);
+            return new \Google\Service\Drive($client);
         });
     }
 }
